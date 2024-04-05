3f618ea42db0af4cfb1449b898525f61a19fb413
Nadiia <nadiahecko@gmail.com>
2024-04-05 15:57:07 +0300

    add

--- src/2.ts
+++ src/2.ts
@@ -14,13 +14,36 @@
 
 */
 
-class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+// class Employee {
+//   // Заповніть модифікатори доступу
+//   name: string;
+//   department: string;
+//   salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+//   constructor(name: string, department: string, salary: number) {
+//     this.name = name;
+//     this.department = department;
+//     this.salary = salary;
+//   }
+
+//   getEmployeeDetails() {
+//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+//   }
+// }
+
+// class Manager extends Employee {
+//   // Реалізуйте конструктор та збільшіть salary на 10000
+// }
+
+
+// export {};
+
+class Employee {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number,
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -32,8 +55,9 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
 
 export {};
