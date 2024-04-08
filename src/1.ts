ccb91b813472e53a5850c024d4def8d16c637df5
Nadiia <nadiahecko@gmail.com>
2024-04-08 13:27:07 +0300

    add

--- src/1.ts
+++ src/1.ts
@@ -20,9 +20,6 @@
 
 class Student {
   constructor(public name: string, public age: number, public grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
   }
 }
 
--- src/4.ts
+++ src/4.ts
@@ -34,12 +34,11 @@ class Person {
 }
 
 abstract class House {
-  protected door: boolean;
+  protected door: boolean = false;
   protected key: Key;
   protected tenants: Person[] = [];
 
   constructor(key: Key) {
-    this.door = false;
     this.key = key;
   }
 
